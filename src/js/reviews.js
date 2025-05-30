c839f44247d07b236b2c54a45bc28623c3fd552f
IhorMtr <igormotornyyy@gmail.com>
2025-05-30 23:12:43 +0300

    almost finished section

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,16 +1,15 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
-window.addEventListener('DOMContentLoaded', () => {
-  var swiper = new Swiper('.marquee-swiper', {
-    slidesPerView: 'auto', // Slides take up the width defined in our CSS
-    spaceBetween: 160, // Adds breathing room between logos
-    loop: true, // If it doesn’t loop, what are we even doing here?
-    speed: 6000, // Time in ms for one slide to transition. Change this to your liking.
-    allowTouchMove: false, // It’s a marquee, not an interactive carnival ride
-    autoplay: {
-      delay: 1, // 1 millisecond delay: we’re moving nonstop. Try 0 here too :).
-      disableOnInteraction: false, // If someone tries to touch, let them fail in peace
-    },
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('button[data-rev-pagination]');
+  btn.addEventListener('click', openPagination);
+
+  function openPagination() {
+    const block = document.querySelector('div[data-rev-visibility]');
+    block.classList.add('visible');
+
+    btn.classList.add('hidden');
+    btn.removeEventListener(openPagination);
+  }
 });
