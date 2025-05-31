bfaa0f2ac477ecd4ac5d806a03d80fec4224c1c7
Anna Gandzulevich <gandzulevich@gmail.com>
2025-05-31 21:00:22 +0300

    add faq

---
+++ src/js/faq.js
@@ -0,0 +1,27 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// new Accordion('.accordion', {
+//   duration: 300,
+//   showMultiple: false,
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordion = new Accordion('.accordion', {
+    duration: 300,
+    showMultiple: false,
+    openOnInit: [0],
+    elementClass: 'accordion-item',
+    triggerClass: 'accordion-header',
+    panelClass: 'accordion-content',
+    activeClass: 'active',
+    beforeOpen: currentElement => {
+      const items = document.querySelectorAll('.accordion-item');
+      items.forEach(item => {
+        if (item !== currentElement) {
+          item.classList.remove('active');
+        }
+      });
+    },
+  });
+});
