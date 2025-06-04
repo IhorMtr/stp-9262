64970f2ad9a6d1ea9b6a43af17ce6540262a50e8
Anna Gandzulevich <gandzulevich@gmail.com>
2025-06-04 20:13:32 +0300

    fix title

--- src/js/home.js
+++ src/js/home.js
@@ -1,18 +1,39 @@
+// Импорт jQuery
+import $ from 'jquery';
+
+// Импорт Lettering.js (он требует jQuery)
+import 'lettering.js';
+
+// Импорт CircleType
+import CircleType from 'circletype';
+
 $(document).ready(function () {
-  $('#curved-text').lettering();
+  const isDesktop = window.innerWidth >= 1200;
 
-  new CircleType(document.getElementById('curved-text'))
-    .radius(2000) // мягкая дуга
-    .dir(1); // направление вниз
+  if (isDesktop) {
+    new CircleType(document.getElementById('curved-text-desktop'))
+      .radius(2000)
+      .dir(1);
 
-  $('#curved-text span').css({
-    margin: '0 -6px',
-    'letter-spacing': '-2px',
-  });
+    $('#curved-text-desktop span').css({
+      margin: '0 -6px',
+      'letter-spacing': '-2px',
+    });
+  } else {
+    setTimeout(() => {
+      new CircleType(document.getElementById('curved-text-line-one'))
+        .radius(1800)
+        .dir(1);
 
-  $('#curved-text span').each(function (i) {
+      new CircleType(document.getElementById('curved-text-line-two'))
+        .radius(1800)
+        .dir(1);
+    }, 50);
+  }
+
+  // Цвета букв + настройка пробелов
+  $('[id^="curved-text"] span').each(function () {
     const letter = $(this).text();
-
     if (letter === 'P' || letter === 'E') {
       $(this).css('color', '#4ff7ff');
     } else if (letter === 'U') {
@@ -21,6 +42,14 @@ $(document).ready(function () {
       $(this).css('color', '#99ed58');
     } else if (letter === 'L') {
       $(this).css('color', '#f7d7d1');
+    }
+
+    // Пробелы
+    if (letter === '\u00A0') {
+      $(this).css({
+        display: 'inline-block',
+        width: '10px',
+      });
     }
   });
 });
