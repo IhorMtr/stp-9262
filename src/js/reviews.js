def094df6fdff2d7c17cbf36867b9668df188bf3
IhorMtr <igormotornyyy@gmail.com>
2025-06-05 03:56:05 +0300

    removed unnecessary code

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,34 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   if (window.matchMedia('(max-width: 767px)').matches) {
-//     return;
-//   }
-
-//   const marqueeContainer = document.querySelector('[data-marquee-container]');
-//   const marqueeContentList = document.querySelector('[data-animate-list]');
-//   if (!marqueeContainer || !marqueeContentList) {
-//     return;
-//   }
-
-//   const containerWidth = marqueeContainer.clientWidth;
-
-//   let listWidth = 0;
-//   Array.from(marqueeContentList.children).forEach(el => {
-//     listWidth += el.offsetWidth;
-//   });
-
-//   const itemsCount = marqueeContentList.children.length;
-//   if (itemsCount === 0) {
-//     return;
-//   }
-
-//   const spaceBetween = (containerWidth - listWidth) / itemsCount;
-
-//   const allItems = document.querySelectorAll('[data-animate-item]');
-//   allItems.forEach(el => {
-//     el.style.marginRight = `${spaceBetween}px`;
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('button[data-rev-pagination]');
   btn.addEventListener('click', openPagination);
